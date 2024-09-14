a83bfbec4ca3cf6c2bba7370c38261b1a7f2ac2e
Berkay Girgin <28847330+Gerile3@users.noreply.github.com>
2024-09-14 11:12:03 +0300

    add initial logic for the calculator

---
+++ script.js
@@ -0,0 +1,103 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const screen = document.querySelector(".screen")
+
+const buttonClick = (value) => {
+    if (isNaN(parseInt(value))){
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    rerender();
+}
+
+const handleNumber = (value) => {
+    if (buffer === "0"){
+        buffer = value
+    } else {
+        buffer += value;
+    }
+}
+
+const handleMath = (value) => {
+    if (buffer === "0"){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+    buffer = "0";
+}
+
+const flushOperation = (intBuffer) => {
+    switch (previousOperator) {
+        case "+":
+            runningTotal += intBuffer;
+            break;
+        case "-":
+            runningTotal -= intBuffer;
+            break;
+        case "×":
+            runningTotal *= intBuffer;
+            break;
+        case "÷":
+            runningTotal /= intBuffer;
+            break;
+        default:
+            break;
+    }
+}
+
+const handleSymbol = (value) => {
+    switch (value) {
+        case "C":
+            buffer = "0"
+            runningTotal = 0
+            break;
+        case "=":
+            if (previousOperator === null){
+                return;
+            }
+            flushOperation(parseInt(buffer))
+            previousOperator = null
+            buffer = +runningTotal
+            runningTotal = 0
+            break;
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0"
+            } else {
+                buffer = buffer.substring(0,buffer.length - 1)
+            }
+            break;
+        case "+":
+        case "-":
+        case "×":
+        case "÷":
+            handleMath(value)
+            break;
+        default:
+            break;
+    }
+}
+
+const rerender = () => {
+    screen.innerText = buffer;
+}
+
+const init = () => {
+    document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", (event) => {
+        buttonClick(event.target.innerText)
+    })
+}
+
+init()
